face90e1f5ac8299831973597977b00980fbb70c
AbdElrahmanGbr <abdelrahman.gbr.os@gmail.com>
2022-04-12 22:42:31 +0200

    create button update

--- Day1/resources/views/posts/index.blade.php
+++ Day1/resources/views/posts/index.blade.php
@@ -4,14 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Latest Posts</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">    
 </head>
 <body>
     <h1 class="text-center">DashBoard For Posts</h1>
     <div class="text-center">
+        <a href="{{ route('posts.create') }}" class="mt-4 btn btn-success">Create Post</a>
+        <!-- <a href="/posts/create" class="mt-4 btn btn-success">Create Post</a> -->
 <div class="container">
-    <button class="mt-4 btn btn-success">Create Post</button>
     <div class="table-responsive">
     <table class="table mt-4 align-middle table-bordered border-primary">
         <thead class="table-dark">
