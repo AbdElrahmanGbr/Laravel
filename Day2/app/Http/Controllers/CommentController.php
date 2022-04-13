56e25921001cd8f9eddf1920c543f01123f93639
AbdElrahmanGbr <abdelrahman.gbr.os@gmail.com>
2022-04-13 21:57:12 +0200

    functions of comment controller

--- Day2/app/Http/Controllers/CommentController.php
+++ Day2/app/Http/Controllers/CommentController.php
@@ -9,9 +9,36 @@ class CommentController extends Controller
     //
     public function create($postId)
     {
-        $post = Post::find($postId);
-        return view('comments.create', [
-            'post' => $post,
+        $post = Post::findOrFail($postId);
+        $req = request();
+        $post->Comment()->create([
+            'user_id' => $req->user_id,
+            'body' => $req->body,
+            'commentable_id' => $postId,
+            'commentable_type' => 'App\Models\Post',
         ]);
+        return redirect()->route('posts.show', $postId);
+    }
+    public function view($postId, $commentId)
+    {
+        $post = Post::findOrFail($postId);
+        $comment = Comment::where('id', $commentId)->first();
+        return view('comments.edit', [
+            'post' => $post,
+            'comment' => $comment,
+        ]);
+    }
+    public function edit($postId, $commentId)
+    {
+        $req = request();
+        Comment::where('id', $commentId)->update([
+            'body' => $req->body,
+        ]);
+        return redirect()->route('posts.show', $postId);
+    }
+    public function delete($postId, $commentId)
+    {
+        Comment::where('id', $commentId)->delete();
+        return redirect()->route('posts.show', $postId);
     }
 }
